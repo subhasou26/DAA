f2ccc9f6d9d3564b692ca92c8c3c94229dc7d359
subha <subhadipp26@gmail.com>
2024-03-20 20:45:06 +0530

    rumorchange

--- complexity/rumor.c
+++ complexity/rumor.c
@@ -13,7 +13,7 @@ double lorec(int n); // Declare lorec before hirec since hirec calls lorec
 
 
 
-double hirec(int n) {
+double hirec(int n) { //2^n
     if (n == 0) return 1;
     return 2 * hirec(n - 1) + lorec(n - 1);
 }
@@ -24,18 +24,42 @@ double lorec(int n) {
 }
 
 two_val hilorec(int n) {
-    two_val N;
-    N.hi = hirec(n);
-    N.lo = lorec(n);
-    return N;
+    two_val ans;
+    double ansh[n+1];
+    double ansl[n+1];
+    ansh[0]=1;
+    ansl[0]=0;
+    for(int i=1;i<=n;i++){
+    	ansh[i]=ansh[i-1]*2+ansl[i-1];
+    	ansl[i]=ansl[i-1]+ansh[i-1];
+    }
+    ans.hi=ansh[n];
+    ans.lo=ansl[n];
+    return ans;
 }
+
+two_val hilorec2(int n) {
+
+if(n==0) {
+	two_val temp;
+	temp.hi=1;
+	temp.lo=0;
+	return temp;
+}
+two_val ans,temp;
+temp=hilorec2(n-1);
+ans.hi=temp.hi*2+temp.lo;
+ans.lo=temp.hi+temp.lo;
+return ans;
+}
+
 
 two_val hiloformula(int n) {
     two_val N;
     N.hi = ((5 + sqrt(5)) / 10) * pow(((3 - sqrt(5)) / 2), n+1) 
      + ((5 - sqrt(5)) / 10) * pow(((3 + sqrt(5)) / 2), n+1);
 
-    N.lo = ((-5 - 5*sqrt(5)) / 10 * pow((3 - sqrt(5)) / 2, n+1)) + 
+    N.lo = ((-5 - 3*sqrt(5)) / 10 * pow((3 - sqrt(5)) / 2, n+1)) + 
       ((-5 + 3*sqrt(5)) / 10 * pow((3 + sqrt(5)) / 2, n+1));
 
     return N;
@@ -51,17 +75,17 @@ int main(int argc, char *argv[]) {
     scanf("%d", &n);
     printf("n = %d\n", n);
 
-    printf("\n+++ Method 0\n");
+   /* printf("\n+++ Method 0\n");
     start = clock();
     N1.hi = hirec(n); N1.lo = lorec(n);
     end = clock();
     printf("    hi(%d) = %.10le, lo(%d) = %.10le\n", n, N1.hi, n, N1.lo);
     cpu_time_used = ((double) (end - start))/CLOCKS_PER_SEC;
-    printf("Method 0 took %f seconds to execute \n", cpu_time_used);
+    printf("Method 0 took %f seconds to execute \n", cpu_time_used);*/
 
     printf("\n+++ Method 1\n");
     start = clock();
-    N2 = hilorec(n);
+    N2 = hilorec2(n);
     end = clock();
     printf("    hi(%d) = %.10le, lo(%d) = %.10le\n", n, N2.hi, n, N2.lo);
     cpu_time_used = ((double) (end - start))/CLOCKS_PER_SEC;
